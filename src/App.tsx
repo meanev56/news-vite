283e66d518c72c1d9fcee4d755c75568de17eaf8
meanev56 <emekanjoku86@gmail.com>
2026-01-01 14:59:31 +0100

    added shadcn and tailwind, added routre to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Article from "./pages/Article";
+import Sports from "./pages/Sports";
+import Banking from "./pages/Banking";
+import Markets from "./pages/Markets";
+import AboutUs from "./pages/AboutUs";
+import NotFound from "./pages/NotFound";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const queryClient = new QueryClient();
 
-export default App
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/article/:slug" element={<Article />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/banking" element={<Banking />} />
+          <Route path="/markets" element={<Markets />} />
+          <Route path="/about" element={<AboutUs />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
